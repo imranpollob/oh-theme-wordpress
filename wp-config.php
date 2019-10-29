d1cf02bcd55bbbf4ab0ad55d2db006ea88123b7e
pollmix <polboy777@gmail.com>
2019-10-29 21:34:56 +0600

    Added dotenv

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,8 @@
 <?php
+// Including Dotenv
+require_once(__DIR__.'/vendor/autoload.php');
+Dotenv\Dotenv::create(__DIR__)->load();
+
 /**
  * The base configuration for WordPress
  *
@@ -20,16 +24,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wppol' );
+define( 'DB_NAME', getenv('DB_NAME') );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', getenv('DB_USER') );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', getenv('DB_HOST') );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
